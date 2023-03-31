9eca2f88800b25796b96710b0dae11283bb1d48a
Tom Longhurst <thomhurst@gmail.com>
2023-03-31 14:09:43 +0100

    Own files

---
+++ TomLonghurst.Nupendencies.Abstractions/Models/TargetFramework.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Build.Construction;
+
+namespace TomLonghurst.Nupendencies.Abstractions.Models;
+
+public record TargetFramework
+{
+    private string? _currentValue;
+    public Project Project { get; }
+    public ProjectPropertyElement? TargetFrameworkTag { get; init; }
+
+    public TargetFramework(Project project, ProjectPropertyElement? targetFrameworkTag)
+    {
+        Project = project;
+        TargetFrameworkTag = targetFrameworkTag;
+        OriginalValue = targetFrameworkTag?.Value;
+        CurrentValue = OriginalValue;
+    }
+
+    public string? OriginalValue { get; }
+
+    public string? CurrentValue
+    {
+        get => _currentValue;
+        set
+        {
+            if (OriginalValue != null)
+            {
+                TargetFrameworkTag!.Value = value;
+                Project.Save();
+                _currentValue = value;
+            }
+        }
+    }
+
+    public void Rollback()
+    {
+        CurrentValue = OriginalValue;
+    }
+    
+    public bool HasValue => !string.IsNullOrWhiteSpace(OriginalValue);
+    
+    public bool IsNetCore => NetCoreParser.IsNetCore(OriginalValue);
+}
---
+++ TomLonghurst.Nupendencies.Abstractions/Models/TargetFrameworks.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Construction;
+
+namespace TomLonghurst.Nupendencies.Abstractions.Models;
+
+public record TargetFrameworks(ProjectPropertyElement? TargetFrameworksTag)
+{
+    public string[] Values => TargetFrameworksTag?.Value?
+        .Split(';')
+        .Select(x => x.Trim())
+        .ToArray() ?? Array.Empty<string>();
+
+    public bool HasValues => Values.Any();
+
+    public bool IsMultiTargeted => Values.Length > 1;
+
+    public bool IsNetCore => Values.Any(NetCoreParser.IsNetCore);
+}
