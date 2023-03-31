cfd3a24a073a47e83fe2554a940dbe2c2cb490a5
Tom Longhurst <thomhurst@gmail.com>
2023-03-31 12:20:19 +0100

    Commit new projects

---
+++ TomLonghurst.Nupendencies.Abstractions/Models/GitRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace TomLonghurst.Nupendencies.Abstractions.Models;
+
+public record GitRepository
+{
+    public required string Owner { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Id { get; set; }
+
+    public required bool IsDisabled { get; set; }
+
+    public required List<GitIssue> Issues { get; set; }
+
+    public required string GitUrl { get; set; }
+
+    public required string MainBranch { get; set; }
+}
