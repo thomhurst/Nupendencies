613a3dae233a4f3e63300f545891f53705f345e7
Tom Longhurst <thomhurst@gmail.com>
2023-04-01 18:12:06 +0100

    set

--- TomLonghurst.Nupendencies/Options/PrivateNugetFeedOptions.cs
+++ TomLonghurst.Nupendencies/Options/PrivateNugetFeedOptions.cs
@@ -2,8 +2,8 @@
 
 public class PrivateNugetFeedOptions
 {
-    public required string Username { get; init; }
-    public required string PatToken { get; init; }
-    public required string SourceName { get; init; }
-    public required string SourceUrl { get; init; }
+    public required string Username { get; set; }
+    public required string PatToken { get; set; }
+    public required string SourceName { get; set; }
+    public required string SourceUrl { get; set; }
 }
