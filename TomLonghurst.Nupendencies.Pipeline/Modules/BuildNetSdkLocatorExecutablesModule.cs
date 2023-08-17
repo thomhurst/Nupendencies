c719a9059e7cbba594f345502c829cc230c40243
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-08-17 20:00:15 +0100

    Build SDK Locator (#7)
    * Build SDK Locator
    
    * Windows latest agent
    
    * Remove shoulds
    
    ---------
    
    Co-authored-by: Tom Longhurst <130671542+msm-tomlonghurst@users.noreply.github.com>

---
+++ TomLonghurst.Nupendencies.Pipeline/Modules/BuildNetSdkLocatorExecutablesModule.cs
@@ -0,0 +1,35 @@
+using ModularPipelines.Context;
+using ModularPipelines.DotNet.Extensions;
+using ModularPipelines.DotNet.Options;
+using ModularPipelines.Models;
+using ModularPipelines.Modules;
+
+namespace TomLonghurst.Nupendencies.Pipeline.Modules;
+
+public class BuildNetSdkLocatorExecutablesModule : Module<List<CommandResult>>
+{
+    protected override async Task<ModuleResult<List<CommandResult>>?> ExecuteAsync(IModuleContext context, CancellationToken cancellationToken)
+    {
+        var projectFile = context.Environment.GitRootDirectory!.GetFiles(f => f.Path.EndsWith("TomLonghurst.Nupendencies.NetSdkLocator.csproj")).First();
+        
+        var results = new List<CommandResult>();
+
+        results.Add(await context.DotNet().Build(new DotNetBuildOptions
+        {
+            TargetPath = projectFile,
+            Configuration = Configuration.Release,
+            Framework = "net48",
+            LogOutput = false,
+        }, cancellationToken));
+        
+        results.Add(await context.DotNet().Build(new DotNetBuildOptions
+        {
+            TargetPath = projectFile,
+            Configuration = Configuration.Release,
+            Framework = "net7.0",
+            LogOutput = false,
+        }, cancellationToken));
+
+        return results;
+    }
+}
